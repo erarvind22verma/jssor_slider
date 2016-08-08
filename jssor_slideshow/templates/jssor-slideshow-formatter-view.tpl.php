95bcdff6aeaa14167f76bb35820fc0d594935a81
arvind <arvind22.verma@gmail.com>
2016-08-07 22:56:33 -0700

    second commit

--- jssor_slideshow/templates/jssor-slideshow-formatter-view.tpl.php
+++ jssor_slideshow/templates/jssor-slideshow-formatter-view.tpl.php
@@ -1,25 +1,27 @@
-<div id="jssor_1" style="position: relative; margin: 0 auto; top: 0px; left: 0px; width: 600px; height: 300px; overflow: hidden; visibility: hidden;">
+<div id="jssor_<?php print $field_id;?>" style="position: relative; margin: 0 auto; top: 0px; left: 0px; width: 600px; height: 300px; overflow: hidden; visibility: hidden;">
         <!-- Loading Screen -->
         <div data-u="loading" style="position: absolute; top: 0px; left: 0px;">
             <div style="filter: alpha(opacity=70); opacity: 0.7; position: absolute; display: block; top: 0px; left: 0px; width: 100%; height: 100%;"></div>
-            <div style="position:absolute;display:block;background:url('img/loading.gif') no-repeat center center;top:0px;left:0px;width:100%;height:100%;"></div>
+            <div style="position:absolute;display:block;background:url('<?php print $path; ?>img/loading.gif') no-repeat center center;top:0px;left:0px;width:100%;height:100%;"></div>
         </div>
         <div data-u="slides" style="cursor: default; position: relative; top: 0px; left: 0px; width: 600px; height: 300px; overflow: hidden;">
+            <?php foreach($items as $delta=>$item): ?>
             <div data-p="112.50" style="display: none;">
-                <img data-u="image" src="img/02.jpg" />
-            </div>
-            <div data-p="112.50" style="display: none;">
-                <img data-u="image" src="img/04.jpg" />
-            </div>
-            <div data-p="112.50" style="display: none;">
-                <img data-u="image" src="img/05.jpg" />
-            </div>
-            <div data-p="112.50" style="display: none;">
-                <img data-u="image" src="img/09.jpg" />
-            </div>
+<?php
+$image = theme(
+  'image_style',
+  array(
+    'path' => $item["uri"],
+    'style_name' => $image_style,
+    'alt' => $item["alt"]?$item["alt"]:"te",
+    'title' => $item["title"]?$item["title"]:"ter",
+  )
+);
+print $image;
+?>            </div>
+            <?php endforeach ;?>
             <a data-u="add" href="http://www.jssor.com" style="display:none">Jssor Slider</a>
-        
-        </div>
+</div>
         <!-- Bullet Navigator -->
         <div data-u="navigator" class="jssorb05" style="bottom:16px;right:16px;" data-autocenter="1">
             <!-- bullet navigator item prototype -->
